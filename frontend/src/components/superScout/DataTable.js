fc9e2a61fa2da106fd6e7a007cfd256476b0ab25
Ethan <Ethanwberk@gmail.com>
2022-02-28 15:46:08 -0700

    Fixed data table

--- frontend/src/components/superScout/DataTable.js
+++ frontend/src/components/superScout/DataTable.js
@@ -6,36 +6,24 @@ function DataTable(){
     const [rawData, setRawData] = useState([])
     const [loading , setLoading] = useState(false)
 
-    // const updateData = () => {
-    //     setRawData(TeamDataService.getAllTeamsFlattenedData())
-    // }
-
-
-
-    useEffect(async () => {
-
+    async function updateData() {
         const response = await TeamDataService.getAllTeamsFlattenedData();
-
         setRawData(response.data);
-        console.log(response.data);
-        setLoading(true);
+    }
 
-    }, []);
+
 
     useEffect(() => {
+        async function getData() {
+            const response = await TeamDataService.getAllTeamsFlattenedData();
+            setRawData(response.data);
+            setLoading(true);
+            setInterval(updateData, 10000);
+        }
+        getData();
 
-
-     // run it, run it
-
-
-
-        // return () => {
-        //    setInterval(updateData, 20000);
-        //     const data = TeamDataService.getAllTeamsFlattenedData();
-        //     console.log(data);
-        //
-        // };
     }, []);
+
 
 
     return(
@@ -45,17 +33,17 @@ function DataTable(){
 
                     <table>
                         <thead>
-                        <tr>
-                            {Object.keys(rawData[0]).map(key => <th>{key}</th>)}
+                        <tr key={"labels"}>
+                            {Object.keys(rawData[0]).map((key) => <th key={key}>{key}</th>)}
                         </tr>
                         </thead>
                         <tbody>
 
                         {rawData.map(
-                            (info)=>{
+                            (info, index)=>{
                                 return(
-                                    <tr>
-                                        {Object.values(info).map(value => <td>{value}</td>)}
+                                    <tr key={index}>
+                                        {Object.values(info).map((value, i) => <td key={i}>{value}</td>)}
                                     </tr>
                                 )
                             }
--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "http://localhost:8000/api/v1/teams/",
-    //baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/teams/",
+    //baseURL: "http://localhost:8000/api/v1/teams/",
+    baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/teams/",
     headers: {
         "Content": "application/json"
     }
