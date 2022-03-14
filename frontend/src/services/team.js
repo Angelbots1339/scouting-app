e81b4b7cee5ce03c0e69920cc9491bcfced357c2
Ethan <Ethanwberk@gmail.com>
2022-03-14 15:47:38 -0600

    Fixed front end for new structure

--- frontend/src/services/team.js
+++ frontend/src/services/team.js
@@ -1,32 +1,32 @@
 import http from '../http-common';
 class TeamDataService {
     getAllTeams(){
-        return http.get('/');
+        return http.get('/teams');
     }
     getAllTeamsFlattenedData(){
-        return http.get('/data/flat');
+        return http.get('/stats');
     }
     getTeam(teamNumber){
-        return http.get(`/${teamNumber}`);
+        return http.get(`/team/${teamNumber}`);
     }
     updateTeam(teamNumber, data){
-        return http.put(`/${teamNumber}`, data);
+        return http.put(`/team/${teamNumber}`, data);
     }
     postTeam(data){
-        return http.post('/', data)
+        return http.post('/team', data)
     }
     deleteTeam(teamNumber){
-        return http.delete(`/${teamNumber}`)
+        return http.delete(`/team/${teamNumber}`)
     }
     getScoutNeededTeams(){
-        return http.get('/team/scoutNeeded');
+        return http.get('/teams/scoutNeeded');
     }
 
     addGame(teamNumber, data){
-        return http.post(`/${teamNumber}/game`, data)
+        return http.post(`/team/${teamNumber}/game`, data)
     }
     addNote(teamNumber, note){
-        return http.post(`/${teamNumber}/notes`, {note: note})
+        return http.post(`/team/${teamNumber}/notes`, {note: note})
     }
 
 
