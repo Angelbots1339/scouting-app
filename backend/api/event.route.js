fdd63897b92ab11ac6174939868d94c1ac55972b
Ethan <Ethanwberk@gmail.com>
2022-03-23 17:47:23 -0600

    Fix api for pitscout

--- backend/api/event.route.js
+++ backend/api/event.route.js
@@ -219,7 +219,7 @@ router.route("/event/:event/teams").get((req, res, next) => {
 router.route("/event/:event/teams/pitScouted").get((req, res, next) => {
     Event.findById(req.params.event)
         .then((event) => {
-            res.send(event.teams.filter(team => team.isPitScouted))
+            res.send(event.teams.filter(team => !team.isPitScouted))
         })
         .catch(next)
 })
