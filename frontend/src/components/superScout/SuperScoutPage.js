ad3aa1533cf1ccf80c52b56b3b7ccb9762bab9e3
Dresden6 <dresdenfriar@icloud.com>
2022-04-16 15:30:33 -0600

    Conditional Navigation based on Screen Size

--- frontend/src/components/superScout/SuperScoutPage.js
+++ frontend/src/components/superScout/SuperScoutPage.js
@@ -1,6 +1,6 @@
 import {Outlet} from "react-router-dom";
 import {Link} from "react-router-dom";
-import {AppBar, Button, Drawer, List, ListItem, ListItemButton, Typography} from "@mui/material";
+import {AppBar, Button, ButtonGroup, Drawer, List, ListItem, ListItemButton, Typography} from "@mui/material";
 import DensitySmallIcon from "@mui/icons-material/DensitySmall";
 import BlurOnIcon from '@mui/icons-material/BlurOn';
 import * as React from "react";
@@ -15,17 +15,17 @@ export default function SuperScoutPage() {
 
 
     // This code can be used for variable size on mobile vs desktop
-    //
-    //
-    // const hasWindow = typeof window !== 'undefined';
-    // const [isScreenBig, setScreenBig] = useState(false);
-    //
-    //
+
+
+    const hasWindow = typeof window !== 'undefined';
+    const [isScreenBig, setScreenBig] = useState(false);
+
+
     useEffect(() => {
 
-        // if (hasWindow) {
-        //     setScreenBig(window.innerWidth > 100 ? true : false);
-        // }
+        if (hasWindow) {
+            setScreenBig(window.innerWidth > 800);
+        }
         setCurrentPage(window.location.pathname);
 
     }, []);
@@ -38,39 +38,62 @@ export default function SuperScoutPage() {
             <div style={{width: "auto", alignItems: "center"}}>
                 <AppBar position="sticky" style={{display: "flex", alignItems: "left", width: "100"}}>
 
+                    {/** For Big Screens **/}
 
-                    <Button size={"small"} variant={"contained"} sx={{cursor: 'pointer', width: 10, m: 2}}
-                            onClick={() => setDrawerState(!drawerState)}>
-                        <DensitySmallIcon/>
-                    </Button>
+                    {isScreenBig &&
+
+                        <ButtonGroup sx={{alignSelf: 'center', m: 2}}>
+                            <Button sx={{cursor: 'pointer'}} variant="contained" component={Link} to={'/'}>
+                                <HomeIcon fontSize='large'/>
+                                <Typography variant={"h6"} sx={{m: 1}}>Home</Typography>
+                            </Button>
+
+                            <Button sx={{cursor: 'pointer'}} variant="contained" component={Link}
+                                    to={'/superScout/teamGrid'}>
+                                <BlurOnIcon/>
+                                <Typography variant={"h6"} sx={{m: 1}}>Team Grid</Typography>
+                            </Button>
+
+                        </ButtonGroup>
+                    }
+
+
+                    {/** For Small Screens **/}
+
+                    {!isScreenBig &&
+                        <Button size={"small"} variant={"contained"} sx={{cursor: 'pointer', width: 10, m: 2}}
+                                onClick={() => setDrawerState(!drawerState)}>
+                            <DensitySmallIcon/>
+                        </Button>}
 
                 </AppBar>
 
-                <Drawer
-                    open={drawerState}
-                    onClick={() => setDrawerState(false)}
-                    onClose={() => setDrawerState(false)}
-                >
+                {!isScreenBig &&
+                    <Drawer
+                        open={drawerState}
+                        onClick={() => setDrawerState(false)}
+                        onClose={() => setDrawerState(false)}>
+                        <List sx={{marginX: '20'}}>
+                            <ListItem>
+                                <ListItemButton sx={{cursor: 'pointer'}} component={Link} to={'/'}>
+                                    <HomeIcon fontSize='large'/>
+                                    <Typography variant={"h4"} sx={{m: 1}}>Home</Typography>
+                                </ListItemButton>
+                            </ListItem>
 
-                    <List sx={{marginX: '20'}}>
-                        <ListItem>
-                            <ListItemButton sx={{cursor: 'pointer'}} component={Link} to={'/'}>
-                                <HomeIcon fontSize='large'/>
-                                <Typography variant={"h4"} sx={{m: 1}}>Home</Typography>
-                            </ListItemButton>
-                        </ListItem>
+                            <ListItem sx={{backgroundColor: currentPage === "/superScout/teamGrid" ? 'darkRed' : ''}}>
+                                <BlurOnIcon/>
+                                <ListItemButton sx={{cursor: 'pointer'}} component={Link} to={'/superScout/teamGrid'}>
+                                    <Typography variant={"h6"} sx={{m: 1}}>Team Grid</Typography>
+                                </ListItemButton>
+                            </ListItem>
 
-                        <ListItem sx={{backgroundColor: currentPage === "/superScout/teamGrid" ? 'darkRed' : ''}}>
-                            <BlurOnIcon/>
-                            <ListItemButton sx={{cursor: 'pointer'}} component={Link} to={'/superScout/teamGrid'}>Team Grid</ListItemButton>
-                        </ListItem>
-
-                    </List>
-                </Drawer>
+                        </List>
+                    </Drawer>
+                }
             </div>
             <Outlet/>
         </div>
-
 
 
     );
