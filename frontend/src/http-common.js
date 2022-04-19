b96ee5ac3f4a70d99b26df2b118bdf4c59632bbd
Ethan <Ethanwberk@gmail.com>
2022-04-18 19:04:01 -0600

    switched to new competition

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-    //baseURL: "http://localhost:8000/api/v1/event/2022temp/",
-    baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/event/2022code/",
+    //baseURL: "http://localhost:8000/api/v1/event/2022hop/",
+    baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/event/2022hop/",
     headers: {
         "Content": "application/json"
     }
