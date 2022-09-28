14a7acfd3bd143b16eeeab5c8d1d3b851e362d4c
ethan <ethanwberk@gmail.com>
2022-09-28 16:30:37 -0600

    Added local http requests and switched to 2022cokc

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
     //baseURL: "http://localhost:8000/api/v1/event/2022code/",
-    baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/event/2022iri/",
+    baseURL: "https://scouting-app-angelbotics.herokuapp.com/api/v1/event/2022cokc/",
     headers: {
         "Content": "application/json"
     }
